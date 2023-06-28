1e9c50139df053e3f399f6c1b6c23ca670ee504f
yoonzy-tech <dev.rubyc@gmail.com>
2023-06-28 16:01:25 +0800

    setup project

---
+++ CryptoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoApp
+//
+//  Created by Ruby Chew on 2023/6/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
